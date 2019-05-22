ced683363d01eee595b64a00bcec9f1372036b7d
umair qayyum <uqayyum@ssidecisions.com>
2019-05-22 13:30:58 +0500

    aws_credentials constructors

--- AWS_SUITE/AWS/S3.cs
+++ AWS_SUITE/AWS/S3.cs
@@ -11,17 +11,17 @@ namespace AWS_SUITE
 {
     public class S3
     {
-        AWS_Credentials credentials;
+        AWS_Credentials Credentials;
 
         #region Constructors
         public S3()
         {
-            credentials = new AWS_Credentials();
+            this.Credentials = new AWS_Credentials();
         }
 
         public S3(AWS_Credentials credentials)
         {
-            this.credentials = credentials;
+            this.Credentials = credentials;
         }
         #endregion
     }
--- AWS_SUITE/Models/AWS_Credentials.cs
+++ AWS_SUITE/Models/AWS_Credentials.cs
@@ -12,5 +12,24 @@ namespace AWS_SUITE.Models
         string AWS_AccessKey;
         string AWS_SecretKey;
         RegionEndpoint Region;
+
+        #region Constructors
+        public AWS_Credentials()
+        {
+        }
+
+        public AWS_Credentials(string access_key, string secret_key)
+        {
+            this.AWS_AccessKey = access_key;
+            this.AWS_SecretKey = secret_key;
+        }
+
+        public AWS_Credentials(string access_key, string secret_key, RegionEndpoint region)
+        {
+            this.AWS_AccessKey = access_key;
+            this.AWS_SecretKey = secret_key;
+            this.Region = region;
+        }
+        #endregion
     }
 }
