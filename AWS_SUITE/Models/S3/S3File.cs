7ed8f619f9f39e9e9fee4b19f7ac90d15262734f
umair qayyum <uqayyum@ssidecisions.com>
2019-05-22 13:55:13 +0500

    S3File

---
+++ AWS_SUITE/Models/S3/S3File.cs
@@ -0,0 +1,30 @@
+﻿
+/**
+* @author Umair Qayyum
+*
+* @date - 5/22/2019 1:51:05 PM 
+*/
+
+namespace AWS_SUITE.Models.S3
+{
+    public class S3File
+    {
+        public string Bucket { get; set; }
+        public string LocalFilePath { get; set; }
+        public string RemoteFilePath { get; set; }
+
+        #region Constructors
+        public S3File()
+        {
+        }
+
+        public S3File(string bucket, string local_path, string remote_path)
+        {
+            this.Bucket = bucket;
+            this.LocalFilePath = local_path;
+            this.RemoteFilePath = remote_path;
+        }
+        #endregion
+
+    }
+}
