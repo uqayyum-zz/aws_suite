7511dfd7f46cfa6a2274e32456e73bdf3f4b80ce
umair qayyum <uqayyum@ssidecisions.com>
2019-05-22 13:24:45 +0500

    aws_credentials region endpoint

--- AWS_SUITE/Models/AWS_Credentials.cs
+++ AWS_SUITE/Models/AWS_Credentials.cs
@@ -1,16 +1,16 @@
-﻿
+﻿using Amazon;
+
 /**
 * @author Umair Qayyum
 *
 * @date - 5/22/2019 1:12:59 PM 
 */
-
 namespace AWS_SUITE.Models
 {
     public class AWS_Credentials
     {
         string AWS_AccessKey;
         string AWS_SecretKey;
-        string Region;
+        RegionEndpoint Region;
     }
 }
